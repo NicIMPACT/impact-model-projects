df4ce37b3c6d79b08666885df24f03ba74969a7e
Drew Bollinger <drew@developmentseed.org>
2014-12-12 13:12:32 -0500

    fix bad array formatting

--- options/scenarios.js
+++ options/scenarios.js
@@ -1,8 +1,7 @@
 scenarios(
 [ {"name": "SSP2-GFDL", "value": "ssp2_gfdl"},
-{"name": "SSP2-HGEM", "value": "ssp2_hgem"}
-{"name": "SSP2-MIROC", "value": "ssp2_miroc"}
-{"name": "SSP2-IPSL", "value": "ssp2_ipsl"}
-{"name": "SSP2-NoCC", "value": "ssp2_nocc"},
-]
+{"name": "SSP2-HGEM", "value": "ssp2_hgem"},
+{"name": "SSP2-MIROC", "value": "ssp2_miroc"},
+{"name": "SSP2-IPSL", "value": "ssp2_ipsl"},
+{"name": "SSP2-NoCC", "value": "ssp2_nocc"} ]
 )
