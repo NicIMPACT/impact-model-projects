a9e91f72ea39af60093029d9c42eb0d3a7d2dca5
Drew Bollinger <drew@developmentseed.org>
2014-12-12 11:01:25 -0500

    more selection options

---
+++ options/groups.js
@@ -0,0 +1,13 @@
+groups(
+[ { "name": "Animal Products", "value": "animal_products" },
+{ "name": "Cereals", "value": "cereals" },
+{ "name": "Fruits and Vegetables", "value": "fruits_vegetables" },
+{ "name": "Oils", "value": "oils" },
+{ "name": "Food Oils", "value": "food_oils" },
+{ "name": "Oil Seeds", "value": "oils_seeds" },
+{ "name": "Oil Meals", "value": "oil_meals" },
+{ "name": "Other", "value": "other" },
+{ "name": "Pulses", "value": "pulses" },
+{ "name": "Roots and Tubers", "value": "roots_tubers" },
+{ "name": "Sugar", "value": "sugar" } ]
+)
