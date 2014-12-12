64b32115232c5d8119b58214d4c50b9e7c8aa116
Drew Bollinger <drew@developmentseed.org>
2014-12-12 13:11:11 -0500

    new fields, new options

---
+++ options/scenarios.js
@@ -0,0 +1,8 @@
+scenarios(
+[ {"name": "SSP2-GFDL", "value": "ssp2_gfdl"},
+{"name": "SSP2-HGEM", "value": "ssp2_hgem"}
+{"name": "SSP2-MIROC", "value": "ssp2_miroc"}
+{"name": "SSP2-IPSL", "value": "ssp2_ipsl"}
+{"name": "SSP2-NoCC", "value": "ssp2_nocc"},
+]
+)
